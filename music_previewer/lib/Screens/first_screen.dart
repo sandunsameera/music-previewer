8b5e20edfbde64dcbf3fb302d15216785ef7d897
sandunsameera <sandunsameera25@gmail.com>
2020-03-20 08:42:59 +0530

    Implement list of songls

--- music_previewer/lib/Screens/first_screen.dart
+++ music_previewer/lib/Screens/first_screen.dart
@@ -1,4 +1,6 @@
 import 'dart:convert';
+import 'package:music_previewer/Screens/single_track.dart';
+import 'package:music_previewer/utils/network_dataparser.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -12,7 +14,7 @@ class _FirstScreenState extends State<FirstScreen> {
   bool isSearching = false;
 
   TextEditingController appBarController = TextEditingController();
-  String apiUrl = "https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem";
+  String apiUrl = "https://deezerdevs-deezer.p.rapidapi.com/search?q=arman malik";
 
   var convertJsonToData;
   http.Response respose;
@@ -22,7 +24,6 @@ class _FirstScreenState extends State<FirstScreen> {
       "Content-Type": "application/json",
       "x-rapidapi-key": "5df91adb64msh026a9441410fb94p1e0a92jsn9512838ab7d5",
     });
-    //  convertJsonToData['data'] = json.decode(respose.body);
 
     if (this.mounted) {
       setState(() {
@@ -40,18 +41,18 @@ class _FirstScreenState extends State<FirstScreen> {
   }
 
   _launchURL(url) async {
-  if (await canLaunch(url)) {
-    await launch(url);
-  } else {
-    throw 'Could not launch $url';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
-}
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
+          backgroundColor: Colors.black,
           title: !isSearching
               ? Text("Welcome to music-previewer")
               : TextField(
@@ -92,43 +93,59 @@ class _FirstScreenState extends State<FirstScreen> {
             convertJsonToData.length != null &&
             convertJsonToData.length > 0
         ? ListView.builder(
+            scrollDirection: Axis.vertical,
+            shrinkWrap: true,
             itemCount:
                 convertJsonToData.length != null ? convertJsonToData.length : 0,
             itemBuilder: (BuildContext context, int index) {
-              return Container(
-                child: Column(
-                  children: <Widget>[
-                    ListTile(
-                      onTap: (){
-                        _launchURL(convertJsonToData[index]['preview']);
-                      },
-                        leading: ClipOval(
-                          child: Image.network(
-                            convertJsonToData[index]['artist']
-                                        ['picuture_small'] !=
-                                    null
-                                ? convertJsonToData[index]['artist']
-                                    ['picuture_small']
-                                : "",
-                            fit: BoxFit.cover,
-                            width: 90.0,
-                            height: 90.0,
-                          ),
-                        ),
+              return InkWell(
+                splashColor: Colors.indigo,
+                onTap: () {
+                  DataParser.trackId = convertJsonToData[index]['id'];
+                  DataParser.albumId = convertJsonToData[index]['album']['id'];
+                  DataParser.artistId = convertJsonToData[index]['artist']['id'];
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => SingleTrack()));
+                },
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        height: MediaQuery.of(context).size.height / 4,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: NetworkImage(convertJsonToData[index]
+                                    ['album']['cover_xl']),
+                                fit: BoxFit.fitWidth)),
+                      ),
+                      ListTile(
+                        leading: Text("Song titile : ",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold)),
                         title: Text(
-                          convertJsonToData[index]['title'] != null
-                              ? convertJsonToData[index]['title']
-                              : "Loading",
-                        )),
-                  ],
+                          convertJsonToData[index]['title'],
+                        ),
+                        trailing: IconButton(
+                            icon: Icon(
+                              Icons.play_circle_filled,
+                              color: Colors.indigo,
+                            ),
+                            onPressed: () => _launchURL(
+                                convertJsonToData[index]['preview'])),
+                      )
+                    ],
+                  ),
                 ),
               );
-            },
-          )
+            })
         : Container(
-          child: Center(
-            child: CircularProgressIndicator(),
-          ),
-        );
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
   }
 }
