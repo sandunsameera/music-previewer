135ae67c612cac9d0dd7ecb4094fa0d76fc9da09
sandunsameera <sandunsameera25@gmail.com>
2020-03-24 00:08:45 +0530

    Implement in app playing

--- music_previewer/lib/Screens/first_screen.dart
+++ music_previewer/lib/Screens/first_screen.dart
@@ -1,9 +1,14 @@
+import 'dart:async';
 import 'dart:convert';
+import 'package:audioplayer/audioplayer.dart';
 import 'package:music_previewer/Screens/single_track.dart';
+import 'package:music_previewer/Screens/test.dart';
 import 'package:music_previewer/utils/network_dataparser.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+
+enum PlayerState { stopped, playing, paused }
 
 class FirstScreen extends StatefulWidget {
   @override
@@ -13,8 +18,27 @@ class FirstScreen extends StatefulWidget {
 class _FirstScreenState extends State<FirstScreen> {
   bool isSearching = false;
   String title = "Previx";
-
   TextEditingController appBarController = TextEditingController();
+
+  Duration duration;
+  Duration position;
+
+  AudioPlayer audioPlayer;
+
+  PlayerState playerState = PlayerState.stopped;
+
+  get isPlaying => playerState == PlayerState.playing;
+  get isPaused => playerState == PlayerState.paused;
+
+  get durationText =>
+      duration != null ? duration.toString().split('.').first : '';
+  get positionText =>
+      position != null ? position.toString().split('.').first : '';
+
+  bool isMuted = false;
+
+  StreamSubscription _positionSubscription;
+  StreamSubscription _audioPlayerStateSubscription;
 
   var convertJsonToData;
   http.Response respose;
@@ -25,6 +49,69 @@ class _FirstScreenState extends State<FirstScreen> {
     // this.appBarController.text = "";
     this.getResult(
         "https://deezerdevs-deezer.p.rapidapi.com/search?q=${DataParser.query}");
+    initAudioPlayer();
+  }
+
+  @override
+  void dispose() {
+    _positionSubscription.cancel();
+    _audioPlayerStateSubscription.cancel();
+    audioPlayer.stop();
+    super.dispose();
+  }
+
+  void initAudioPlayer() {
+    audioPlayer = new AudioPlayer();
+    _positionSubscription = audioPlayer.onAudioPositionChanged
+        .listen((p) => setState(() => position = p));
+    _audioPlayerStateSubscription =
+        audioPlayer.onPlayerStateChanged.listen((s) {
+      if (s == AudioPlayerState.PLAYING) {
+        setState(() => duration = audioPlayer.duration);
+      } else if (s == AudioPlayerState.STOPPED) {
+        onComplete();
+        setState(() {
+          position = duration;
+        });
+      }
+    }, onError: (msg) {
+      setState(() {
+        playerState = PlayerState.stopped;
+        duration = new Duration(seconds: 0);
+        position = new Duration(seconds: 0);
+      });
+    });
+  }
+
+  void onComplete() {
+    setState(() => playerState = PlayerState.stopped);
+  }
+
+  Future play(url) async {
+    await audioPlayer.play(url);
+    setState(() {
+      playerState = PlayerState.playing;
+    });
+  }
+
+  Future pause() async {
+    await audioPlayer.pause();
+    setState(() => playerState = PlayerState.paused);
+  }
+
+  Future stop() async {
+    await audioPlayer.stop();
+    setState(() {
+      playerState = PlayerState.stopped;
+      position = new Duration();
+    });
+  }
+
+  Future mute(bool muted) async {
+    await audioPlayer.mute(muted);
+    setState(() {
+      isMuted = muted;
+    });
   }
 
   Future<Map<String, dynamic>> getResult(apiUrl) async {
@@ -51,30 +138,29 @@ class _FirstScreenState extends State<FirstScreen> {
   }
 
   void _onLoading() {
-  showDialog(
-    context: context,
-    barrierDismissible: false,
-    builder: (BuildContext context) {
-      return Dialog(
-        child: new Row(
-          children: [
-            new Text("Searching your query.."),
-            new CircularProgressIndicator(),
-          ],
-        ),
-      );
-    },
-  );
-  new Future.delayed(new Duration(seconds: 5), () {
-    Navigator.pop(context);
-  });
-}
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return Dialog(
+          child: new Column(
+            children: [
+              new Text("Searching your query.."),
+              new CircularProgressIndicator(),
+            ],
+          ),
+        );
+      },
+    );
+    new Future.delayed(new Duration(seconds: 5), () {
+      Navigator.pop(context);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          
           centerTitle: true,
           backgroundColor: Color(0xFF192A56),
           title: !isSearching
@@ -94,7 +180,7 @@ class _FirstScreenState extends State<FirstScreen> {
                     icon: Icon(Icons.cancel),
                     onPressed: () {
                       //-------------------
-                     _onLoading();
+                      _onLoading();
                       //-------------------
                       setState(() {
                         this.isSearching = false;
@@ -150,7 +236,7 @@ class _FirstScreenState extends State<FirstScreen> {
                     DataParser.artistId =
                         convertJsonToData[index]['artist']['id'];
                     Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => SingleTrack()));
+                        MaterialPageRoute(builder: (context) => HomePage()));
                   },
                   child: Card(
                     shape: RoundedRectangleBorder(
@@ -177,8 +263,10 @@ class _FirstScreenState extends State<FirstScreen> {
                                 Icons.play_circle_filled,
                                 color: Colors.indigo,
                               ),
-                              onPressed: () => _launchURL(
-                                  convertJsonToData[index]['preview'])),
+                              onPressed: () {
+                                // play(convertJsonToData[index]['preview']);
+                                play(convertJsonToData[index]['preview']);
+                              }),
                         )
                       ],
                     ),
